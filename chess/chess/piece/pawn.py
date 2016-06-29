609e3270b494a0a8fa8469fdb2ea8d1019ea910e
foxfluff <foxfluff@users.noreply.github.com>
2016-06-29 13:05:18 -0400

    Implemented legal_moves()

--- chess/chess/piece/pawn.py
+++ chess/chess/piece/pawn.py
@@ -2,5 +2,11 @@
 from ._piece import chess_piece
 
 class pawn(chess_piece):
-    
-    
+
+    def avaliable_moves(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def legal_moves():
+        return [[x, 1] for x in range(-1, 2)] + [[0, 2]]
+
